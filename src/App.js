59104c815ec72cc2e5e1944295b26158f705487d
Hedda <hedda.langvik@gmail.com>
2019-10-10 12:36:36 +0200

    #15 basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Main from './pages/Main';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="App" >
+      <Main />
     </div>
   );
 }
