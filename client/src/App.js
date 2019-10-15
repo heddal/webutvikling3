0d32c1391b69266783f547135a4b62f6be4205a2
Amalie Henni <amalie.henni@gmail.com>
2019-10-15 14:55:38 +0200

    #3 connect frontend backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Card from './components/Card';
 import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App" >
+      <Card />
       <Main />
     </div>
   );
--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express');
-//const bodyParser = require('body-parser')
-//const path = require('path');
 const app = express();
 
 app.get('/api/customers', (req, res) => {
-  // res.send('pling')
+  // Dette er eksempelkode
   const costumers = [
-    //GraphQL ?
     {id: 1, firstName: 'St-st-stol', lastName: 'Veig'},
     {id: 2, firstName: 'Hehehe', lastName: 'dda'},
     {id: 3, firstName: 'Ama', lastName: 'Lamalie'},
     {id: 4, firstName: 'World browser', lastName: 'Find your dream destination'}
   ];
-  res.json(costumers)
+  res.json(costumers);
 });
+
+app.get('/places', (req, res) => {
+  // eksempel på steder
+  const places = [
+    {id: 1,
+      name: 'Paris',
+      continent: 'Europe',
+      temprature: [-5, 30],
+      mood: ['shopping', 'love'], //+ keywords?
+      popularity: 0 // antall besøk her ? ev local storage?
+    }, {id: 2,
+      name: 'Bali',
+      continent: 'Asia',
+      temperature: [10, 40],
+      mood: ['adventure', 'chill', 'beach'],
+      popularity: 0
+    }
+  ]
+  res.json(places);
+})
 
 const port = 5000;
 
