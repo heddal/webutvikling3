3e395b67f5ad7aacffe7827826e64e529899719f
Hedda <hedda.langvik@gmail.com>
2019-10-25 13:10:34 +0200

    #22 backarrow is wrong arg

--- client/src/components/BackButton.js
+++ client/src/components/BackButton.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './BackButton.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
@@ -10,16 +9,24 @@ class BackButton extends Component {
 
     constructor(props){
         super(props);
-        this.former = ""
+        this.former = "search"
     }
     
     //backarrow to go back to right path considering where you are
-    handleClick = () => prop =>{
-        console.log(this.props.location.pathname);
-        if (this.props.location.pathname === "/search"){
+    handleClick = ()  => {
+        console.log("former pathname: " + this.props.location.pathname);
+        const path = this.props.location.pathname;
+        if (path.includes("search")){
+            
             this.former = "/"
         } 
-        else {this.former = "/search"}
+        else {
+            if(this.former === "search"){
+                this.former= "/";
+            }
+            this.former = "/search"
+        }
+        console.log("firmer: " +this.former)
     };
 
     
@@ -27,7 +34,7 @@ class BackButton extends Component {
 
     render(){
         return ( 
-            <Link to={this.former} ><button className = "back-button" onClick={this.handleClick}> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#999999" viewBox="0 0 18 18"><path d="M15 8.25H5.87l4.19-4.19L9 3 3 9l6 6 1.06-1.06-4.19-4.19H15v-1.5z"/></svg> </button> </Link>
+            <Link to={this.former} ><button className = "back-button" onClick={this.handleClick.bind(this)}> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#999999" viewBox="0 0 18 18"><path d="M15 8.25H5.87l4.19-4.19L9 3 3 9l6 6 1.06-1.06-4.19-4.19H15v-1.5z"/></svg> </button> </Link>
         );
     }
 
